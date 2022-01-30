7306383c11c86b7f046bc5b72010f0472f3acc55
Danila Ilyin <nonier1108@gmail.com>
2022-01-30 05:35:36 +0300

    init film_service module

---
+++ film_service/src/main/java/com/example/film_service/FilmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.film_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmServiceApplication.class, args);
+	}
+
+}
---
+++ film_service/src/test/java/com/example/film_service/FilmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.film_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
