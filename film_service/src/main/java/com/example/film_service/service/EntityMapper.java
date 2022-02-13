5cae9ea733cdcf6db0ee2eb8492b8a9fbc4f123a
Danila Ilyin <nonier1108@gmail.com>
2022-02-13 12:17:45 +0300

    create EntityMapper

---
+++ film_service/src/main/java/com/example/film_service/service/EntityMapper.java
@@ -0,0 +1,77 @@
+package com.example.film_service.service;
+
+import com.example.film_service.dto.ActorDto;
+import com.example.film_service.dto.ActorRoleDto;
+import com.example.film_service.dto.FilmDto;
+import com.example.film_service.dto.GenreDto;
+import com.example.film_service.entity.Actor;
+import com.example.film_service.entity.ActorRole;
+import com.example.film_service.entity.Film;
+import com.example.film_service.entity.Genre;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper
+public interface EntityMapper {
+
+    @Mappings({
+            @Mapping(target = "firstName", source = "actor.firstName"),
+            @Mapping(target = "secondName", source = "actor.secondName"),
+            @Mapping(target = "age", source = "actor.age"),
+            @Mapping(target = "actorRoles", source = "actor.actorRoles")
+    })
+    ActorDto actorToActorDto(Actor actor);
+
+    @Mappings({
+            @Mapping(target = "firstName", source = "actorDto.firstName"),
+            @Mapping(target = "secondName", source = "actorDto.secondName"),
+            @Mapping(target = "age", source = "actorDto.age"),
+            @Mapping(target = "actorRoles", source = "actorDto.actorRoles")
+    })
+    Actor actorDtoToActor(ActorDto actorDto);
+
+    @Mappings({
+            @Mapping(target = "name", source = "film.name"),
+            @Mapping(target = "description", source = "film.description"),
+            @Mapping(target = "releaseDate", source = "film.releaseDate"),
+            @Mapping(target = "duration", source = "film.duration"),
+            @Mapping(target = "genres", source = "film.genres"),
+            @Mapping(target = "actorRoles", source = "film.actorRoles")
+    })
+    FilmDto filmToFilmDto(Film film);
+
+    @Mappings({
+            @Mapping(target = "name", source = "filmDto.name"),
+            @Mapping(target = "description", source = "filmDto.description"),
+            @Mapping(target = "releaseDate", source = "filmDto.releaseDate"),
+            @Mapping(target = "duration", source = "filmDto.duration"),
+            @Mapping(target = "genres", source = "filmDto.genres"),
+            @Mapping(target = "actorRoles", source = "filmDto.actorRoles")
+    })
+    Film filmDtoToFilm(FilmDto filmDto);
+
+    @Mappings({
+            @Mapping(target = "role", source = "actorRole.role"),
+            @Mapping(target = "actor", source = "actorRole.actor"),
+            @Mapping(target = "film", source = "actorRole.film")
+    })
+    ActorRoleDto actorRoleToActorRoleDto(ActorRole actorRole);
+
+    @Mappings({
+            @Mapping(target = "role", source = "actorRoleDto.role"),
+            @Mapping(target = "actor", source = "actorRoleDto.actor"),
+            @Mapping(target = "film", source = "actorRoleDto.film")
+    })
+    ActorRole actorRoleDtoToActorRole(ActorRoleDto actorRoleDto);
+
+    @Mappings({
+            @Mapping(target = "name", source = "genre.name")
+    })
+    GenreDto genreToGenreDto(Genre genre);
+
+    @Mappings({
+            @Mapping(target = "name", source = "genreDto.name")
+    })
+    Genre genreDtoToGenre(GenreDto genreDto);
+}
